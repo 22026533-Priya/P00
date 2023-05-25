a3f58a203b8f9938211840b8d3461d40904c5b46
Priyanka <Priyanka@10.174.121.115>
2023-05-25 11:44:55 +0800

    first push to P00 repository in GitHub

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -16,4 +16,5 @@ public class Calculator {
 		}
 //testing
 	//testing
+	//testing
 }
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -66,6 +66,7 @@ class CalculatorTest {
 	}
 //testing
 	//testing
+	//testing
 	
 	@Test
 	public void testDivide() {
